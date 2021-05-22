c98637df9fa009efe2d39be20ce6031cfdd88412
Pavlo <konoplovpavlo@gmail.com>
2021-05-22 21:04:43 +0300

    Promise Part I

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,8 +18,10 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.dish = this.dishService.getFeaturedDish();
-    this.promotion = this.promotionService.getFeaturedPromotion();
+    this.dishService.getFeaturedDish()
+      .then(dish => this.dish = dish);
+    this.promotionService.getFeaturedPromotion()
+      .then(promotion => this.promotion = promotion);
   }
 
 }
